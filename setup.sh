fdfafb581722f5e8f2df822e5eb388f81aab2dcb
John Hinnegan <john.hinnegan@gmail.com>
2010-10-07 09:30:13 -0700

    Updated readme and setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,33 @@
 #!/bin/zsh
 
+currDir=`pwd`
+echo "Present directory is $currDir"
+
 echo "Enter the directory where infinitered dotfiles is located: "
+echo "(Don't include the trailing slash cause I'm too lazy to be smart about this)"
 read directory 
 
-ln -s ./vimrc.local ~/vimrc.local 
-ln -s ./zshrc ~/.zshrc 
-ln -s ./zshrc.cmdprompt ~/.zshrc.cmdprompt 
-ln -s $directory/etc/vim/gvimrc ~/.gvimrc 
-ln -s $directory/etc/vim ~/.vim 
-mv ./ir_black.vim $directory/etc/vim/colors/ir_black.vim
+ln -sf $currDir/zshrc ~/.zshrc 
+echo "created symlink to zshrc"
+
+ln -sf $currDir/zshrc.cmdprompt ~/.zshrc.cmdprompt 
+echo "created symlink to zshrc.cmdprompt"
+
+# prepare the vimrc
+cp $directory/etc/vim/vimrc $currDir/vimrc 
+sed '/.*LongLineWarning.*/d' $currDir/vimrc > $currDir/vimrc.new 
+mv $currDir/vimrc.new $currDir/vimrc
+ln -sf $currDir/vimrc ~/.vimrc
+echo "created symlink to vimrc in infinitered folder"
+
+ln -sf $currDir/vimrc.local ~/vimrc.local 
+echo "Created symlink to vimrc.local"
+
+ln -sf $directory/etc/vim/gvimrc ~/.gvimrc 
+echo "created symlink to gvimrc in infinitered folder"
+
+ln -sf $directory/etc/vim ~/.vim 
+echo "created symlink to .vim in infinitered folder"
+
+cp -f $currDir/ir_black.vim $directory/etc/vim/colors/ir_black.vim
+echo "overwrite ir_black color with the good one"
